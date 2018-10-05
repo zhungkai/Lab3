e1cf998c85117669483efe0b7f4ce0ea19781a2c
zhungkai <y299935226@gmail.con>
2018-10-05 17:50:36 +0800

    fk

---
+++ Lab3/Lab3/maic.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int  main(void)
+{
+	int num1;
+	int num2;
+
+	printf("Enter two  integers, and I will tell you\n");
+	printf("the relationships they  satisfy: ");
+
+	scanf_s("%d%d" , &num1 , &num2);
+
+	if (num1 == num2)
+	{
+		printf("%d is equal to %d\n", num1 , num2);
+	}
+	if (num1 != num2)
+	{
+		printf("%d is not  eqaul to %d\n", num1, num2);
+	}
+	if (num1 < num2)
+	{
+		printf("%d is greater than %d\n", num1, num2);
+	}    
+	if (num1 > num2)
+	{
+		printf("%d is more than %d\n", num1, num2);   
+	}
+	if (num1 >= num2)
+	{
+		printf("%d is greater than or equal to %d\n", num1, num2);
+	}
+	if (num1 <= num2)
+	{
+		printf("%d is less than or equal to %d\n ", num1, num2);
+	}
+	system("pause");
+	return 0;
+}
